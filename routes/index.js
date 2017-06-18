4a414882714dad1bc15fa0568ed5e6b4356e1ca8
kmicic91 <bilicz.adrian@gmail.com>
2017-06-17 20:32:19 -0400

    handle the unix format

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+router.get('/:time', function(req,res){
+
+	function unixToNatural(unix){
+
+		var date = moment(unix).format('LL')
+		return date
+	}
+
+	if (!isNaN(req.params.time)) {
+		var result = unixToNatural(req.params.time);
+		var data = {unix: req.params.time, natural: result}
+		res.json(data)
+	}
+})
 
 module.exports = router;
